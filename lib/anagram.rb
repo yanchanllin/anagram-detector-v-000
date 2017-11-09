329390dfccbb87590feb844217815ed62cbbfac6
YanC Lin <yonalin1@gmail.com>
2017-11-09 22:04:47 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,10 +7,10 @@ class Anagram
   end
 
   def match
-    word.detect do |some_word|
       puts "Must find #{self.word} inside the following string:
       #{some_word}"
       # ALL << some_word.match(%w(some_word))
+       some_word.find_all do |word|
       if some_word.split("").sort == self.word.split("").sort
         word
       end
