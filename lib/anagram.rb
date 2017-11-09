9fe16c8f870f0ef5c6d0560cb722dc8438b4f28d
YanC Lin <yonalin1@gmail.com>
2017-11-09 22:08:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,17 +1,17 @@
 class Anagram
-  attr_accessor :word
+  attr_accessor :word_to_find
 
 
   def initialize(word)
-    @word = word
+    @word_to_find = word_to_find
   end
 
   def match(some_word)
-      puts "Must find #{self.word} inside the following string:
+      puts "Must find #{self.word_to_find} inside the following string:
       #{some_word}"
       # ALL << some_word.match(%w(some_word))
        some_word.find_all do |word|
-      if some_word.split("").sort == self.word.split("").sort
+      if word.split("").sort == self.word_to_find.split("").sort
         word
       end
     end
