30d949d024258146a8f4839c7753f6472aa0870f
YanC Lin <yonalin1@gmail.com>
2017-11-09 22:12:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,11 +5,12 @@ class Anagram
     @word_to_analyze = word_to_analyze
   end
 
-  def match(find_anagram_in_this_string)
-    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
-    find_anagram_in_this_string.find_all do |word|
+  def match(find_anagram)
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram}"
+    find_anagram.find_all do |word|
       if word.split("").sort == self.word_to_analyze.split("").sort
         word
+        
       end
     end
   end
